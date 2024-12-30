df70f985b25480f53796e9bae6e282acc01b6b84
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-30 15:34:46 +0500

    Make the outlets for the storay board and import the mapkit and coreLocation library

--- MapKitDemoUIKit/ViewController.swift
+++ MapKitDemoUIKit/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
